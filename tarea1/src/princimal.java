b480c771608c44797fd7a7d45944f61f73d3273e
CarlosRamirez2003 <109763655+CarlosRamirez2003@users.noreply.github.com>
2022-07-22 11:44:56 -1200

    Add files via upload

---
+++ tarea1/src/princimal.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class princimal {
+    public static void main(String[] args) {
+        Scanner leer = new Scanner(System.in);
+         String  nombre;
+        System.out.println("ingrese nombre " );
+        nombre= leer.nextLine();
+        System.out.println();
+        System.out.println("Bienvenido a Progra 2 2022"+nombre);
+
+    }
+}
